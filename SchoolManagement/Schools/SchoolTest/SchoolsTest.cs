959b635911b32cc51b8ca9e009ef5bf5c69dae5f
keerthana <keerthananeeluru2001@gmail.com>
2023-09-20 08:01:29 +0530

    adding section 2

---
+++ SchoolManagement/Schools/SchoolTest/SchoolsTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Schools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolTest
+{
+    [TestFixture]
+    public class SchoolsTest
+    {
+        
+        [Test]
+        public void NotNullTest()
+        {
+            School sch=new School();
+            sch.SchoolName = "Narayana";
+            sch.SchoolBranch = "Hyd";
+            sch.Fee = 0;
+            Assert.NotNull(sch.SchoolName);
+            Assert.NotNull(sch.SchoolBranch);
+            Assert.NotNull(sch.Fee);
+        }
+       [Test]
+        public void SchooolId()
+        {
+            School sch=new School();
+            sch.Id = 1;
+            int sid1 = 1,sid2=3;
+            Assert.That(sid1, Is.EqualTo(sch.Id));
+            
+        }
+    }
+}
---
+++ SchoolManagement/Schools/Schools/School.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schools
+{
+    public class School
+    {
+        public int Id { get; set; }
+        public string SchoolName { get; set; }
+        public string SchoolBranch { get; set; }
+        public double Fee { get; set; }
+
+    }
+}
