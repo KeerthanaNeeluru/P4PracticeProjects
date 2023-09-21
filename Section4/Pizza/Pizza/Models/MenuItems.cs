248475506bd9e3735f4770c1fb57bc2ffccd73f6
keerthana <keerthananeeluru2001@gmail.com>
2023-09-21 10:44:32 +0530

    adding section 4

---
+++ Section4/Pizza/Pizza/Models/MenuItems.cs
@@ -0,0 +1,10 @@
+﻿namespace Pizza.Models
+{
+    public class MenuItems
+    {
+        public int Id { get; set; }
+        public string ItemName { get; set; }
+        public string Toppings { get; set; }
+        public double Price {  get; set; }
+    }
+}
