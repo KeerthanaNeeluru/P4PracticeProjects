82b5c39cde47f3a5a9bc162178e9e54887c5f0ad
keerthana <keerthananeeluru2001@gmail.com>
2023-09-21 10:21:38 +0530

    adding section 7

---
+++ Section7/FirstBank/FirstBank/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using FirstBank.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstBank.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly Users user=new Users()
+        {
+            Id = 1,
+            UserName = "Sam",
+            Email = "sam@gmail.com",
+            Password = "sam@123"
+        };
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(Users model)
+        {
+            if (ModelState.IsValid && model.UserName == user.UserName && model.Email == user.Email && model.Password == user.Password)
+            {
+                return RedirectToAction("Index", "Dashboard");
+            }
+            ModelState.AddModelError("", "Invalid username or Password");
+            return View(model);
+        }
+    }
+}
